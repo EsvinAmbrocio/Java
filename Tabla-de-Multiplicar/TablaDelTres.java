7c3fbcda85838a867701afabd6bb6a0a95697015
Esvin Ambrocio <esvin01cainedpp@gmail.com>
2019-09-04 06:34:03 -0600

    0.01: Creado el primer documento de tablas de mutiplicar con ciclo for

---
+++ Tabla-de-Multiplicar/TablaDelTres.java
@@ -0,0 +1,17 @@
+public class TablaDelTres {
+
+
+    public static void main(String[] args) {
+        int j=1;
+    do{
+        
+        int i=1;
+        while(i<11){
+            System.out.println(j+" * " +i+" = "+i*j);
+            i++;
+        }
+        j++;
+    }
+       while(j<11);
+   } 
+}
